9c0c5c9f0f96a8faa9d10cc8cd4ad3520ac67620
Tomii313 <tomidiazmoreno@gmail.com>
2025-11-06 21:13:08 -0300

    Commit Después de MYSQL

--- rentarg/settings.py
+++ rentarg/settings.py
@@ -85,10 +85,20 @@ WSGI_APPLICATION = 'rentarg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
+        'ENGINE': 'django.db.backends.mysql',
+       # 'NAME': BASE_DIR / "db.sqlite3",
+        'NAME': 'rosariorent',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    
+    'OPTIONS': {
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
+
+}}
 
 
 # Password validation
